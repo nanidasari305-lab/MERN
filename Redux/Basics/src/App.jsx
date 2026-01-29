5d90501aca20295883b6f98831edf2c000e77f4c
nanidasari305-lab <nanidasari305.com>
2026-01-29 19:27:43 +0530

    1/29

--- Redux/Basics/src/App.jsx
+++ Redux/Basics/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Counter from './components/Counter'
+import Posts from './components/Post'
 
 const App = () => {
   return (
     <div>
       <Counter/>
+      <Posts/>
     </div>
   )
 }
--- Redux/Basics/src/redux/store.jsx
+++ Redux/Basics/src/redux/store.jsx
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from './slices/counterSlice';
-const store=configureStore({
-    reducer:{
-        counter:counterReducer
-    }
+import postSlice from './slices/postSlice'
+
+const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+    posts:postSlice
+  }
 });
+
 export default store;
